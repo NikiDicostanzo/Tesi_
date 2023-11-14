bee2c2b5e303428e9c00d5570644a64d1010e0f2
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2023-11-14 21:37:21 +0100

    Fix merge.

--- overlap_box.py
+++ overlap_box.py
@@ -5,9 +5,8 @@ import os
 from PIL import Image, ImageDraw as D
 import numpy as np
 
-   
-#477 674 ->h-80= 594 -- w / h -> 0.803
-#490 634 -> h-20..  w / h -> 0.798 
+
+# Covert w, h, and center to bb
 def get_bbox_coords(c0, c1, h, w, width, height):
     x_min = (c0 - w / 2) * width
     y_min = (c1 - h / 2) * height
@@ -15,14 +14,13 @@ def get_bbox_coords(c0, c1, h, w, width, height):
     y_max = (c1 + h / 2) * height
     return (x_min, y_min, x_max, y_max)
 
-
+# Get bb from labels of detection
 def get_bb(file_path, image):
 
     file = open(file_path)
     bb = []
     line = []
     type = []
-
     width, height = image.size 
     for i in file:
         line = i.split("\n")[0].split(" ")
@@ -34,22 +32,7 @@ def get_bb(file_path, image):
         bb.append(get_bbox_coords(c0, c1, h, w, width, height))
         type.append(int(line[0]))
     return bb, type
-   
-def draw_bb(bb, image, labels, path_save):
-#def draw_bb(bb, path_image, labels, path_save):
-    # [x0, y0, x1, y1]
-    #image = Image.open(path_image)
-    draw = D.Draw(image)
-    for k in range(len(bb)):
-        if labels[k] == 0:
-           color = 'red'
-        else:
-           color = 'black'
-        draw.rectangle(bb[k], outline = color, width = 1) 
-    if path_save !='':
-        image.save(path_save)
-    else:
-        image.show()
+
     
 """ |----------------- box1 -----------------|
     |                                    |
@@ -68,9 +51,19 @@ def check_overlap(box1, box2):
         box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
         iou = intersect_area / (box1_area + box2_area - intersect_area)
         return iou
+   
+
+def draw_bb(bb, image, labels):
+    draw = D.Draw(image)
+    for k in range(len(bb)):
+        if labels[k] == 0:
+           color = 'red'
+        else:
+           color = 'black'
+        draw.rectangle(bb[k], outline = color, width = 1) 
 
 
-def marge_bb(bb,image,path_save, labels, coord):
+def marge_bb(bb, labels):
   #  path_image = 'dataset/imgs/stat_21_2109.00464_vis/7.png' 
     tmp = []
     remove = []
@@ -98,12 +91,11 @@ def marge_bb(bb,image,path_save, labels, coord):
 
                 remove.append(j) # salvo indici di bb che hanno fatto merge
                 #####TODO Stessa classe
-                marge_box = True    
+                marge_box = True                    
             j +=1
-
         if marge_box : 
-            if [x0,y0,x1,y1] not in tmp:
-                tmp.append([x0,y0,x1,y1])      
+            #if [x0,y0,x1,y1] not in tmp:
+            tmp.append([x0,y0,x1,y1])   
             marge_box = False
         else:
             tmp.append(bb[k])
@@ -115,12 +107,36 @@ def marge_bb(bb,image,path_save, labels, coord):
             new_bb.append(tmp[i])
             new_labes.append(labels[i])
     return new_bb, new_labes
-  
 
+# Merge su una immagine
+def get_bb_merge(path_image, txt, path_save, detect):
+    image = Image.open(path_image)
+    bb, labels = get_bb(txt,image)
+
+    sorted_bb_y, sorted_labels_y  = sort_data(bb, labels, 1)
+    bb_y, labels_y = marge_bb(list(sorted_bb_y), list(sorted_labels_y)) #y
+
+    sorted_bb_x, sorted_labels_x = sort_data(bb_y, labels_y, 0)
+    new_bb, new_labels = marge_bb(list(sorted_bb_x), list(sorted_labels_x)) #x
+    draw_bb(new_bb, image, new_labels)
+    if path_save != '':
+        if detect != '': ## Per visualizzare in un'unica immagine
+            ###
+            image_detect = Image.open(detect) # immagine di yolo
+            dst = get_concat_h(image, image_detect)  
+            dst.save(path_save)
+        else:
+            image.save(path_save)
+    else:
+        image.show()
+    return new_bb, new_labels
+
+# Merge su tutte le immagini
 def merge_all_image(folder):
     # txt e img devono avere stesso nome!
     path_images = folder + 'images/'
     path_txt = folder + 'labels/'
+    images_detect = folder + 'detect/'
 
     dir_list = os.listdir(path_images) 
     num = 0
@@ -132,9 +148,11 @@ def merge_all_image(folder):
         path_image = path_images + d 
         name = '.'.join(d.split('.')[0:len(d.split('.'))-1])
         txt = path_txt + name + '.txt'
+
+        detect = images_detect + name +'.png'
         
         save_image = path_save + d        
-        new_bb, new_labels = get_bb_merge(path_image, txt, save_image)
+        new_bb, new_labels = get_bb_merge(path_image, txt, save_image, detect)
         num +=1
 
 
@@ -145,20 +163,15 @@ def sort_data(bb, labels, index):
     return sorted_bb, sorted_labels
 
 
-def get_bb_merge(path_image, txt, path_save):
-    image = Image.open(path_image)
-    bb, labels = get_bb(txt,image)
-
-    sorted_bb_y, sorted_labels_y  = sort_data(bb, labels, 1)
-    bb_y, labels_y = marge_bb(list(sorted_bb_y), image, '', list(sorted_labels_y), 'y') #y
-
-    sorted_bb_x, sorted_labels_x = sort_data(bb_y, labels_y, 0)
-    new_bb, new_labels = marge_bb(list(sorted_bb_x), image, '', list(sorted_labels_x), 'x') #x
-    if path_save != '':
-        draw_bb(new_bb, image, new_labels, path_save)
-    return new_bb, new_labels
+# TODO da mettere in utils
+def get_concat_h(im1, im2):
+    dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+    dst.paste(im1, (0, 0))
+    dst.paste(im2, (im1.width, 0))
+    return dst
 
 
+# TODO da mettere in utils
 def create_folder(file_path):
     if not os.path.exists(file_path):
         os.makedirs(file_path)
@@ -168,6 +181,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="...")
    # parser.add_argument("--video", dest="video", default=None, help="Path of the video")
     #folder = 'exp_test/'
-    folder = 'C:/Users/ninad/Desktop/ACL_P10-1160_exp2/'
+    folder = 'C:/Users/ninad/Desktop/Ok_test_exp2_stat_21_2109.00464_vis/'
     merge_all_image(folder)
-
