cecb526a0fcda357413a8828385b352406316016
NikiDiCostnazo <niki.dicostanzo@stud.unifi.it>
2024-02-21 13:01:23 +0100

    In_features ok.

--- train.py
+++ train.py
@@ -1,45 +1,57 @@
-from GatModel import GATModel 
 import torch
-from graph import get_graph
+from create_graphGT import get_one_g, get_graphs
 import os
+import torch.nn.functional as F
 
+from model import Model
 
-def my_graph():
-    json_file = 'data_h/json/ACL_2020.acl-main.99.json'
-    path_save = 'data_h/save/'
-    folder = 'data_h/image/'
-    list_image = os.listdir(folder)
-   # print(list_image)
-    #for page in range(len(list_image)):
-    page = 0
-    path_image = folder + list_image[page]
-    image_save = path_save + list_image[page]
-    #get_near(json_file, page, path_image, image_save)
-    g = get_graph(json_file, page, path_image, image_save)
-    return g
+# Eseguire train del modello (devo crearlo )
+# Batch dei grafi !!
+# ... 
+# Ora faccio su GT 
+# Poi devo fare su YOLO (prima devo fare mapping !!! e embedding testo)
 
-def main():
-    #model = GATModel(input_dim, output_dim, num_heads)
-    graph = my_graph()
-
-    node_features = graph.ndata['centroids']#.float()
-    edge_label = graph.edata['label']#.long()
-
-    #node_features = edge_pred_graph.ndata['feature']
-    #edge_label = edge_pred_graph.edata['label']
-    #train_mask = edge_pred_graph.edata['train_mask']
-
-    model = GATModel(2, 23, 4)
+# Graph(num_nodes=902, num_edges=1895,
+#       ndata_schemes={'page': Scheme(shape=(), dtype=torch.int64)}
+#       edata_schemes={'label': Scheme(shape=(), dtype=torch.int64)})
+# Provo il modello su GT 
+def model_train(graph):
+    print(graph)
+    node_features = graph.ndata['page'].float()
+    edge_label = graph.edata['label']
+    print(node_features)
+    # in_feature = data.node_num_classes
+    out_features = 2 
+    hidden = 20
+    #model = Model(graph.num_nodes, hidden , out_features)
+    inputs = node_features.unsqueeze(-1)
+    print(inputs)
+    model = Model(1, 32,5)
+    edge_label = edge_label.unsqueeze(-1)
     opt = torch.optim.Adam(model.parameters())
     for epoch in range(10):
-        pred = model(graph, node_features)
-        #loss = ((pred[train_mask] - edge_label[train_mask]) ** 2).mean()
+        pred = model(graph, inputs)         
         loss = ((pred - edge_label) ** 2).mean()
-
         opt.zero_grad()
         loss.backward()
         opt.step()
         print(loss.item())
-    
+
+def main():
+    graph = get_one_g()#get_graphs()
+    model_train(graph)
+
 if __name__ == '__main__':
     main()
+
+   # print(get_one_g())
+    
+""" 
+
+tuo tensore di input dovrebbe avere una dimensione di (num_nodes, in_feats) 
+dove num_nodes è il numero di nodi nel tuo grafo e in_feats è il numero totale 
+di caratteristiche per nodo.
+Poiché 'bb' ha 4 valori e 'page' ha 1 valore, il numero totale di caratteristiche per nodo sarà 5
+Per preparare l'input corretto, devi combinare i tensori delle caratteristiche 'bb' e 'page' 
+in un unico tensore
+"""
